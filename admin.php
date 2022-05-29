452d0d4b8fdbdce0b07c245ed5541ddc86839af8
Otto Hooper <votton.hooper@gmail.com>
2022-05-29 17:10:24 +0100

    Register, login and admin page

---
+++ admin.php
@@ -0,0 +1,16 @@
+<?php
+// If $_SESSION['username'] is set, then the user is logged in
+if (isset($_SESSION['username'])) {
+  echo "Welcome, " . $_SESSION['username'] . "!";
+} else {
+    echo "You are not logged in";
+    // Redirect back to login page
+    header('Location: login.php');
+}
+
+?>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload" name="submit">
+</form>
---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+// Login to website
+if (isset($_POST['username']) && isset($_POST['password'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+  if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  }
+  if (!$res = $mysqli->query("SELECT * FROM `users` WHERE `username` = '$username'")) {
+    echo "Failed to retrieve user: " . $mysqli->error;
+  }
+  $user = $res->fetch_object();
+  if ($user) {
+    if (password_verify($password, $user->password)) {
+      $_SESSION['username'] = $username;
+      $_SESSION['user_id'] = $user->id;
+      header('Location: index.php');
+    } else {
+      echo "Incorrect password";
+    }
+  } else {
+    echo "User does not exist";
+  }
+}
+?>
+
+<form action="login.php" method="post">
+  <input type="text" name="username" placeholder="Username">
+  <input type="password" name="password" placeholder="Password">
+  <input type="submit" value="Login">
+</form>
