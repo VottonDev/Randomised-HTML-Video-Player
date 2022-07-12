65d4876889753bbcdfe92138f76ca6571aa1dfff
Restyled.io <commits@restyled.io>
2022-07-12 15:53:18 +0000

    Restyled by clang-format

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 function getColor(start, end, percent) {
-  function hex2dec(hex) {
-    return parseInt(hex, 16);
-  }
+  function hex2dec(hex) { return parseInt(hex, 16); }
 
-  function dec2hex(dec) {
-    return (dec < 16 ? "0" : "") + dec.toString(16);
-  }
+  function dec2hex(dec) { return (dec < 16 ? "0" : "") + dec.toString(16); }
 
-  const r1 = hex2dec(start.slice(0, 2)),
-    g1 = hex2dec(start.slice(2, 4)),
-    b1 = hex2dec(start.slice(4, 6));
-  const r2 = hex2dec(end.slice(0, 2)),
-    g2 = hex2dec(end.slice(2, 4)),
-    b2 = hex2dec(end.slice(4, 6));
+  const r1 = hex2dec(start.slice(0, 2)), g1 = hex2dec(start.slice(2, 4)),
+        b1 = hex2dec(start.slice(4, 6));
+  const r2 = hex2dec(end.slice(0, 2)), g2 = hex2dec(end.slice(2, 4)),
+        b2 = hex2dec(end.slice(4, 6));
   const pc = percent / 100;
   const r = Math.floor(r1 + pc * (r2 - r1) + 0.5),
-    g = Math.floor(g1 + pc * (g2 - g1) + 0.5),
-    b = Math.floor(b1 + pc * (b2 - b1) + 0.5);
+        g = Math.floor(g1 + pc * (g2 - g1) + 0.5),
+        b = Math.floor(b1 + pc * (b2 - b1) + 0.5);
   return "#" + dec2hex(r) + dec2hex(g) + dec2hex(b);
 }
 
@@ -56,6 +50,4 @@ function fadeAll() {
   fadeSpan();
 }
 
-function JSFX_StartEffects() {
-  fadeAll("h1", "h2", "h3");
-}
+function JSFX_StartEffects() { fadeAll("h1", "h2", "h3"); }
